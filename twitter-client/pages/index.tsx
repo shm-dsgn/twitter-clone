ddbe5fbc2253114332028ee4df617a7c8b1355c5
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2024-01-29 01:50:13 +0530

    Added tweet modal

--- twitter-client/pages/index.tsx
+++ twitter-client/pages/index.tsx
@@ -8,6 +8,7 @@ import {
   BookmarkSimple,
   User,
   DotsThree,
+  ImageSquare,
 } from "@phosphor-icons/react/dist/ssr";
 import FeedCard from "@/components/FeedCard/FeedCard";
 import { GoogleLogin, CredentialResponse } from "@react-oauth/google";
@@ -65,6 +66,14 @@ export default function Home() {
   const { user } = useCurrentUser();
   const queryClient = useQueryClient();
 
+  const handleSelectImage = useCallback(() => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
+
+    input.click();
+  }, []);
+
   const handleLoginWithGoogle = useCallback(
     async (cred: CredentialResponse) => {
       const googleToken = cred.credential;
@@ -111,7 +120,10 @@ export default function Home() {
           {user && (
             <div className="flex items-center gap-4 p-3 hover:bg-gray-900 cursor-pointer transition-all rounded-full h-fit w-fit absolute bottom-2">
               <Image
-                src={user?.profileImageURL || "https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png"}
+                src={
+                  user?.profileImageURL ||
+                  "https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png"
+                }
                 alt="profile"
                 className="rounded-full"
                 width={36}
@@ -126,6 +138,38 @@ export default function Home() {
           )}
         </div>
         <div className="col-span-6 border-x-[0.5px] border-gray-800 h-screen overflow-y-scroll no-scrollbar">
+          <div>
+            <div className="border border-x-0 border-b-0 border-gray-900 p-4 ">
+              <div className="grid grid-cols-12">
+                <div className="col-span-1">
+                  {user?.profileImageURL && (
+                    <Image
+                      src={user?.profileImageURL}
+                      alt="profile"
+                      className="rounded-full"
+                      height={50}
+                      width={50}
+                    />
+                  )}
+                </div>
+                <div className="col-span-11 pl-4">
+                  <textarea
+                    className=" w-full bg-transparent focus:outline-none border-b-2 border-slate-700"
+                    rows={3}
+                    placeholder="What's happening?"
+                  ></textarea>
+                  <div className="text-blue-500 flex justify-between items-center">
+                    <div className="p-2 hover:bg-gray-900 cursor-pointer transition-all rounded-full h-fit w-fit" onClick={handleSelectImage}>
+                      <ImageSquare size={20} />
+                    </div>
+                    <button className="bg-blue-500 hover:bg-blue-600 transition-all rounded-full py-2 px-4 h-fit text-white text-xs">
+                      Tweet
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <FeedCard />
           <FeedCard />
           <FeedCard />
